f0466892567d9619ffe17557671ad701ad9f4642
Steve Carroll <stevencarroll90@gmail.com>
2025-07-17 22:35:14 +0000

    Adding bootstrapped frontend code with empty page

---
+++ frontend/src/App.tsx
@@ -0,0 +1,9 @@
+import RecordList from './pages/RecordList/RecordList';
+
+export default function App() {
+  return (
+    <div className="records-container">
+      <RecordList/>
+    </div>
+  );
+}
