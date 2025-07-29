495a107cfb8d67ff12bb783464ceed175c27c932
Steve Carroll <stevencarroll90@gmail.com>
2025-07-29 16:49:00 +0000

    Refactoring to handle responses better. Adding a "remove domain" route.

--- backend/internal/pihole/cluster.go
+++ backend/internal/pihole/cluster.go
@@ -10,23 +10,81 @@ func NewCluster(clients ...ClientInterface) *Cluster {
 	return &Cluster{clients: clients}
 }
 
-func (c *Cluster) FetchLogs(opts FetchLogsQueryOptions) ([]*QueryLogResponse, []error) {
+func errorString(err error) string {
+	if err != nil {
+		return err.Error()
+	}
+	return ""
+}
+
+func (c *Cluster) FetchQueryLogs(opts FetchQueryLogOptions) []*NodeResult[FetchQueryLogResponse] {
 	var wg sync.WaitGroup
-	results := make([]*QueryLogResponse, len(c.clients))
-	errs := make([]error, len(c.clients))
+	results := make([]*NodeResult[FetchQueryLogResponse], len(c.clients))
 
 	for i, client := range c.clients {
 		wg.Add(1)
 		go func(i int, ci ClientInterface) {
 			defer wg.Done()
-			r, err := ci.FetchLogs(opts)
-			results[i] = r
-			errs[i] = err
+			r, err := ci.FetchQueryLogs(opts)
+			node := ci.GetNodeInfo()
+			results[i] = &NodeResult[FetchQueryLogResponse]{
+				PiholeNode: node,
+				Success:    err == nil,
+				Error:      errorString(err),
+				Response:   r,
+			}
 		}(i, client)
 	}
 
 	wg.Wait()
-	return results, errs
+	return results
+}
+
+func (c *Cluster) AddDomainRule(opts AddDomainRuleOptions) []*NodeResult[AddDomainRuleResponse] {
+	var wg sync.WaitGroup
+	results := make([]*NodeResult[AddDomainRuleResponse], len(c.clients))
+
+	for i, client := range c.clients {
+		wg.Add(1)
+		go func(i int, ci ClientInterface) {
+			defer wg.Done()
+			r, err := ci.AddDomainRule(opts)
+			node := ci.GetNodeInfo()
+			results[i] = &NodeResult[AddDomainRuleResponse]{
+				PiholeNode: node,
+				Success:    err == nil,
+				Error:      errorString(err),
+				Response:   r,
+			}
+		}(i, client)
+	}
+
+	wg.Wait()
+	return results
+}
+
+func (c *Cluster) RemoveDomainRule(opts RemoveDomainRuleOptions) []*NodeResult[RemoveDomainRuleResponse] {
+	var wg sync.WaitGroup
+	results := make([]*NodeResult[RemoveDomainRuleResponse], len(c.clients))
+
+	for i, client := range c.clients {
+		wg.Add(1)
+		go func(i int, ci ClientInterface) {
+			defer wg.Done()
+			err := ci.RemoveDomainRule(opts)
+			node := ci.GetNodeInfo()
+
+			results[i] = &NodeResult[RemoveDomainRuleResponse]{
+				PiholeNode: node,
+				Success:    err == nil,
+				Error:      errorString(err),
+				// Response is nil because 204 has no body
+			}
+		}(i, client)
+	}
+
+	wg.Wait()
+	return results
 }
 
 func (c *Cluster) Logout() []error {
