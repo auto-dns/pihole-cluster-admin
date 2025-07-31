d6aa6e129a0a00c55b8485478ecda30d16daa431
Steve Carroll <stevencarroll90@gmail.com>
2025-07-31 17:34:44 +0000

    Adding a create user route that only works when the app is uninitialized, creates a session and returns a cookie

--- backend/internal/migrations/001_initial_schema.up.sql
+++ backend/internal/migrations/001_initial_schema.up.sql
@@ -1,8 +1,22 @@
 CREATE TABLE users (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     username TEXT NOT NULL UNIQUE,
-    password_hash TEXT NOT NULL
+    password_hash TEXT NOT NULL,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    updated_at DATETIME DEFAULT CURRENT_TIMESTAMP
 );
+
+CREATE TRIGGER update_users_updated_at
+AFTER UPDATE OF username, password_hash
+ON users
+FOR EACH ROW
+WHEN OLD.username != NEW.username
+    OR OLD.password_hash != NEW.password_hash
+BEGIN
+    UPDATE users
+    SET updated_at = CURRENT_TIMESTAMP
+    WHERE id = OLD.id;
+END;
 
 CREATE TABLE piholes (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
