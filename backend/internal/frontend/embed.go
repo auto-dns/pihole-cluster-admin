bc01ef657f7eb264aff848ca91282721ae74c872
Steve Carroll <stevencarroll90@gmail.com>
2025-07-31 21:20:45 +0000

    Updating to remove proxy configurations

--- backend/internal/frontend/embed.go
+++ backend/internal/frontend/embed.go
@@ -1,19 +1,6 @@
 package frontend
 
-import (
-	"embed"
-	"io/fs"
-	"log"
-	"net/http"
-)
+import "embed"
 
 //go:embed dist/*
-var embeddedFiles embed.FS
-
-func ServeStatic() http.Handler {
-	content, err := fs.Sub(embeddedFiles, "dist")
-	if err != nil {
-		log.Fatalf("Failed to load embedded frontend: %v", err)
-	}
-	return http.FileServer(http.FS(content))
-}
+var Files embed.FS
