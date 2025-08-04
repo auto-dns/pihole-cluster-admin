9368b91a8aec42480ba40f2372123b7fb18a4088
Steve Carroll <stevencarroll90@gmail.com>
2025-08-04 14:35:22 +0000

    Added login form

---
+++ frontend/src/pages/Login.tsx
@@ -0,0 +1,41 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router';
+import { useAuth } from '../app/AuthProvider';
+import useInput from '../lib/hooks/useInput';
+
+export default function Login() {
+    const {user, login} = useAuth();
+    const username = useInput('');
+    const password = useInput('');
+
+    async function submitForm() {
+        try {
+            await login(username.value, password.value);
+        } catch (e: unknown) {
+            if (e instanceof Error) {
+                alert(e.message);
+                console.error(e);
+            } else {
+                alert('Error: see console log for full error output')
+                console.error(e);
+            }
+        }
+    }
+
+    return (
+        <div>
+            <h1>Login</h1>
+            <div>
+                <label>
+                    Username
+                    <input name='username' value={username.value} onChange={username.onChange}/>
+                </label>
+                <label>
+                    Password
+                    <input name='password' type='password' value={password.value} onChange={password.onChange}/>
+                </label>
+                <button onClick={submitForm}>Log In</button>
+            </div>
+        </div>
+    );
+}
