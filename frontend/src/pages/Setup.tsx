73b5e4d4caa9af4c4a8d5de5427f31ddebc6d180
Steve Carroll <scarroll@qualtrics.com>
2025-08-04 22:07:44 +0000

    Trying to fix redirect loop

--- frontend/src/pages/Setup.tsx
+++ frontend/src/pages/Setup.tsx
@@ -2,6 +2,7 @@ import {Navigate} from 'react-router';
 import {useInitializationStatus} from '../app/InitializationStatusProvider';
 
 export default function Setup() {
+    console.log('setup');
     const init = useInitializationStatus();
     
     debugger;
