36acd4fe883323f8558d3cc102bc065e717b22c5
Steve Carroll <stevencarroll90@gmail.com>
2025-08-06 15:51:05 +0000

    Adding next / skip button

--- frontend/src/pages/SetupPiholes.tsx
+++ frontend/src/pages/SetupPiholes.tsx
@@ -3,10 +3,22 @@ import '../styles/pages/pihole-setup.scss';
 
 export default function SetupPiholes() {
 	const { piholeNodes } = usePiholes();
+
+	function handleClick() {
+		if (piholeNodes.length) {
+			console.log('Finish setup');
+		} else {
+			console.log('Skip pihole setup');
+		}
+	}
+
 	return (
 		<div className='pihole-creation-setup'>
 			<div className='setup-card'>
 				<h1>Add one or more Pihole instances to get started</h1>
+				<button onClick={handleClick}>
+					{piholeNodes.length ? 'Finish Setup' : 'Skip'}
+				</button>
 			</div>
 		</div>
 	);
