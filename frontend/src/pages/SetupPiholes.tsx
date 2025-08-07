3888e41bee3ce46c697c465e9270aa76136d18a7
Steve Carroll <stevencarroll90@gmail.com>
2025-08-07 21:43:58 +0000

    Adding a pihole setup page with a button in middle when you have no nodes

--- frontend/src/pages/SetupPiholes.tsx
+++ frontend/src/pages/SetupPiholes.tsx
@@ -1,13 +1,18 @@
-// import { usePiholes } from '../providers/PiholeProvider';
+import PiholeManagementList from '../components/PiholeManagementList';
+import { usePiholes } from '../providers/PiholeProvider';
 import '../styles/pages/pihole-setup.scss';
 
 export default function SetupPiholes() {
-	// const { piholeNodes } = usePiholes();
+	const { piholeNodes } = usePiholes();
 
 	return (
 		<div className='pihole-creation-setup'>
 			<div className='setup-card'>
-				<h1>Add one or more Pihole instances to get started</h1>
+				<h2 className='setup-step-title'>
+					Add one or more Pihole instances to get started
+				</h2>
+				<PiholeManagementList />
+				<button>{piholeNodes?.length ? 'Finish Setup' : 'Skip'}</button>
 			</div>
 		</div>
 	);
