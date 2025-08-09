f356e44564dbdd533ddb748c75140e87b75f5c6d
Steve Carroll <stevencarroll90@gmail.com>
2025-08-09 14:55:30 +0000

    Updates to pihole node form

--- frontend/src/components/PiholeManagementList/PiholeNodeForm.tsx
+++ frontend/src/components/PiholeManagementList/PiholeNodeForm.tsx
@@ -1,7 +1,63 @@
 import { PiholeNode } from '../../types/pihole';
 import useInput from '../../hooks/useInput';
+import { HttpScheme } from '../../types';
+import { FormEvent } from 'react';
 
-export default function PiholeNodeForm({ node }: { node: PiholeNode }) {
-	const [scheme, setScheme] = useInput();
-	return <form></form>;
+interface Props {
+	node?: PiholeNode;
+	onSubmit: (node: PiholeNode, password: string) => void;
+}
+
+export default function PiholeNodeForm({ node, onSubmit }: Props) {
+	const id = node?.id;
+	const scheme = useInput(node?.scheme ?? '');
+	const host = useInput(node?.host ?? '');
+	const port = useInput(node?.port?.toString() ?? '');
+	const name = useInput(node?.name ?? '');
+	const description = useInput(node?.description ?? '');
+	const password = useInput('');
+
+	function handleSubmit(e: FormEvent) {
+		e.preventDefault();
+
+		// Conversion
+		const portNumber = Number(port.value);
+
+		const formNode = {
+			host,
+			port: portNumber,
+			name,
+			description,
+		};
+		onSubmit(formNode, password);
+	}
+
+	return (
+		<form onSubmit={handleSubmit}>
+			<label>
+				Name
+				<input {...name} />
+			</label>
+			<label>
+				Scheme
+				<input {...scheme} />
+			</label>
+			<label>
+				Host
+				<input {...host} />
+			</label>
+			<label>
+				Port
+				<input type='number' min={1} max={65535} {...port} />
+			</label>
+			<label>
+				Password
+				<input type='password' {...password} />
+			</label>
+			<label>
+				Description
+				<input {...description} />
+			</label>
+		</form>
+	);
 }
