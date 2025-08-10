2e8e6b6e97da74aa51b875e1fd27b1fbc81a2af0
Steve Carroll <stevencarroll90@gmail.com>
2025-08-10 20:13:03 +0000

    Stubbing call to test pihole endpoint

--- frontend/src/lib/api/pihole.ts
+++ frontend/src/lib/api/pihole.ts
@@ -1,21 +1,43 @@
 import apiFetch from './client';
-import { PiholeCreateBody, PiholePatchBody, PiholeNode } from '../../types/pihole';
+import { PiholeNode } from '../../types/pihole';
+import { HttpScheme } from '../../types';
+
+type PiholeNodeDraft = Omit<PiholeNode, 'id'>;
 
 export async function getPiholeNodes(): Promise<PiholeNode[]> {
 	return apiFetch<PiholeNode[]>('/piholes');
 }
 
+export type PiholeCreateBody = PiholeNodeDraft & {
+	password: string;
+};
 export async function createPiholeNode(nodeDraft: PiholeCreateBody): Promise<PiholeNode> {
 	return apiFetch<PiholeNode>('/piholes', {
-		body: JSON.stringify({ nodeDraft }),
 		method: 'POST',
+		body: JSON.stringify({ nodeDraft }),
 	});
 }
 
+export type PiholePatchBody = Partial<PiholeNodeDraft> & {
+	password?: string;
+};
 export async function editPiholeNode(id: number, nodeDraft: PiholePatchBody): Promise<PiholeNode> {
 	// TODO: should I escape id?
 	return apiFetch<PiholeNode>(`/piholes/${id}`, {
-		body: JSON.stringify(nodeDraft),
 		method: 'PATCH',
+		body: JSON.stringify(nodeDraft),
+	});
+}
+
+export interface PiholeTestConnectionBody {
+	scheme: HttpScheme;
+	host: string;
+	port: number;
+}
+
+export async function testPiholeConnection(node: PiholeTestConnectionBody) {
+	return apiFetch<void>('/piholes/test', {
+		method: 'POST',
+		body: JSON.stringify(node),
 	});
 }
