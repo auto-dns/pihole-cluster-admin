c2d6addca0688e4ac2b4512868d21441d203cb67
Steve Carroll <stevencarroll90@gmail.com>
2025-08-18 02:33:13 +0000

    Adding styling for app cards and centered pages

---
+++ frontend/src/components/Layout/AppCard.tsx
@@ -0,0 +1,12 @@
+import { ReactNode } from 'react';
+import styles from './AppCard.module.scss';
+import classNames from 'classnames';
+
+type Props = {
+	children: ReactNode;
+	className?: string;
+};
+
+export default function AppCard({ className, children }: Props) {
+	return <div className={classNames(className, styles.card)}>{children}</div>;
+}
