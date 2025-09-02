551513b26ffea5e4bb9d4dd127c1a34a4e9a9383
Steve Carroll <stevencarroll90@gmail.com>
2025-09-02 15:01:53 +0000

    Removing TODOs and adding redirect workflow

--- backend/internal/realtime/topics.go
+++ backend/internal/realtime/topics.go
@@ -2,7 +2,6 @@ package realtime
 
 type Topic = string
 
-// TODO: ChatGPT recommending decoupling v1 from here and introducing it at edge only - search for "SSE topics and versioning"
 const (
 	TopicClusterBlockingV1 Topic = "v1.cluster_blocking"
 	TopicClusterHealthV1   Topic = "v1.cluster_health"
